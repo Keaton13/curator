f8891e804ff6fc39fc46061da43d442cbb6a5441
Keaton13 <bajabuggy313@gmail.com>
2021-10-20 14:41:56 -0700

    Got inital metamask connection working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import logo from './logo.svg';
+import { injected } from './components/wallet/connectors';
 import './App.css';
+import { useWeb3React } from '@web3-react/core';
 
 function App() {
+  const { active, account, library, connector, activate, deactivate } = useWeb3React();
+
+  async function connect() {
+    try {
+      await activate(injected)
+    } catch (ex) {
+      console.log(ex)
+    }
+  }
+
+  async function disconnect() {
+    try {
+      deactivate();
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
   return (
     <div className="App">
-        <h1>Test</h1>
-        <div>
-          <button className="btn btn-outline-primary">Connect To MetaMask</button>
+      <h1>Test</h1>
+      <div className="container mt-4">
+        <div className="row text-center">
+          <button onClick={connect} className="btn btn-outline-primary w-50 mx-auto">Connect To MetaMask</button>
         </div>
+        <div className="row mt-3 mb-3">
+          {active ? <span>Connected with <b>{account}</b></span> : <span>Not Connected</span>}
+        </div>
+        <div className="row">
+          <button onClick={disconnect} className="btn btn-outline-danger w-50 mx-auto">Disconnect</button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Web3ReactProvider } from '@web3-react/core'
+import Web3 from 'web3'
 
+function getLibrary(provider) {
+  return new Web3(provider)
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
