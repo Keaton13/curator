fe1322e3d894b13d503e70d41bbfa3c919d93ffb
Keaton13 <bajabuggy313@gmail.com>
2021-10-26 12:22:48 -0700

    Moving coinmarketcap data to table

--- src/components/ticker.jsx
+++ src/components/ticker.jsx
@@ -41,14 +41,38 @@ class Ticker extends React.Component {
                     {data && data.map(dat => {
                         return (
                             <div className="row">
-                                <div className="col">
+                                <table className="tabe">
+                                    <thread className="thread-dark">
+                                        <tr>
+                                            <th scope="col"></th>
+                                            <th scope="col">Name</th>
+                                            <th scope="col">Price</th>
+                                            <th scope="col">1hr</th>
+                                            <th scope="col">7d</th>
+                                        </tr>
+                                    </thread>
+                                    <tbody>
+                                        {data && data.map(dat => {
+                                            return (
+                                                <tr>
+                                                    <th scope="row"></th>
+                                                    <td>{dat.name}</td>
+                                                    <td>{dat.quote.USD.price.toFixed(2) + '$'}</td>
+                                                    <td>{'1hr ' + dat.quote.USD.percent_change_1h.toFixed(2) + '%'}</td>
+                                                    <td>{'7d ' + dat.quote.USD.percent_change_7d.toFixed(2) + '%'}</td>
+                                                </tr>
+                                            )
+                                        })}
+                                    </tbody>
+                                </table>
+                                {/* <div className="col">
                                     <h4>{dat.name}</h4>
                                     <h5>{dat.quote.USD.price.toFixed(2) + '$'}</h5>
                                 </div>
                                 <div className="col">
                                     <h5>{'1hr ' + dat.quote.USD.percent_change_1h.toFixed(2) + '%'}</h5>
                                     <h5>{'7d ' + dat.quote.USD.percent_change_7d.toFixed(2) + '%'}</h5>
-                                </div>
+                                </div> */}
                             </div>
                         )
                     })}
