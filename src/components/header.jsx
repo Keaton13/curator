a786434a778b37a66885846199e10b8aab9e0932
Keaton13 <bajabuggy313@gmail.com>
2021-12-07 11:23:48 -0800

    Made dropdown close on click

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -22,8 +22,8 @@ const Header = props => {
 
                     </button>
                     <div class={dropDownStatus} aria-labelledby="dropdownMenuButton">
-                        <Link class="no-underline" to="/"><a class="dropdown-item">Balance</a></Link>
-                        <Link class="no-underline" to="ticker"><a class="dropdown-item">Ticker</a></Link>
+                        <Link class="no-underline" to="/" onClick={() => { handleDropdownClick() }}><a class="dropdown-item">Balance</a></Link>
+                        <Link class="no-underline" to="ticker" onClick={() => { handleDropdownClick() }}><a class="dropdown-item">Ticker</a></Link>
                     </div>
                 </div>
                 <h1 className="mt-3 float-right-custom">Curator</h1>
