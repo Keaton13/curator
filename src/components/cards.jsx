c7369361f97e9571f0d539ad0691fd51e45906d3
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2022-04-15 10:05:51 -0700

    Created Card Element

---
+++ src/components/cards.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const cards = () => {
+  return (
+    <div className="row cardLayout mt-3">
+      <div className="col-4">
+        <div className="card">
+
+        </div>
+      </div>
+      <div className="col-4"> 
+        <div className="card">
+
+        </div>
+      </div>
+      <div className="col-4">
+        <div className="card">
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default cards;
