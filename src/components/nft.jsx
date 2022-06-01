44f24d6ae581bc43443638c4d4c7f7a95ec39f05
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2022-05-31 17:22:10 -0700

    Added conditional rendering to NFt

--- src/components/nft.jsx
+++ src/components/nft.jsx
@@ -1,21 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useMoralisWeb3Api } from "react-moralis";
 import { useDispatch, useSelector } from "react-redux";
 import { HandleNftMetaData } from "../redux/actions/web3Actions";
 import { useMoralis } from "react-moralis";
+import Logo from "../images/images.png";
 
 const Nft = () => {
-  useEffect(() => {
-    fetchNFTs();
-  }, []);
-
+  const [status, setStatus] = useState(false);
   const { isAuthenticated } = useMoralis();
 
   const nftMetaData = useSelector(
     (state) => state.web3Reducer.nftMetaData.data
   );
+  console.log(nftMetaData);
   const Web3Api = useMoralisWeb3Api();
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isAuthenticated === true) {
+      if (nftMetaData === null) {
+        fetchNFTs();
+      } else {
+        setStatus(true);
+      }
+    } else {
+      setStatus(false);
+    }
+  }, [isAuthenticated]);
 
   const fetchNFTs = async () => {
     // get NFTs for current user on Mainnet
@@ -23,10 +34,14 @@ const Nft = () => {
 
     // get polygon NFTs for address
     const options = {
-      chain: "polygon"
+      chain: "polygon",
     };
     const polygonNFTs = await Web3Api.account.getNFTs(options);
     dispatch(HandleNftMetaData(userEthNFTs, polygonNFTs));
+
+    if (isAuthenticated === true) {
+      setStatus(true);
+    }
   };
 
   // const testData = async () => {
@@ -39,16 +54,16 @@ const Nft = () => {
   // };
 
   return (
-    <div className="container min-width100">
+    <div className="container min-width100 min-Height100">
       <div className="row mt-3 headerLinks">
         <div className="col nftDisplay">
-          <h3>Porfolio Value</h3>
+          {status === true && <h3>Porfolio Value</h3>}
           <div className="row">
-            {isAuthenticated !== null &  nftMetaData !== null &&
+            {(isAuthenticated === true) & (nftMetaData !== null) ? (
               nftMetaData.map((nft) => {
                 // console.log(nftMetaData)
                 let metaData = JSON.parse(nft.metadata);
-                console.log(metaData)
+                console.log(metaData);
                 return (
                   <div className="col-3 mt-3 mb-4">
                     <div className="container nftDisplayBackground">
@@ -73,7 +88,34 @@ const Nft = () => {
                     </div>{" "}
                   </div>
                 );
-              })}
+              })
+            ) : (
+              <div className="col-3 mt-3 mb-4 mx-auto">
+                <div className="container nftDisplayBackground">
+                  <div className="row">
+                    <div className="col"></div>
+                  </div>
+                  <div className="row mt-3 nftInfoDisplayHeight">
+                    <div className="col text-center">
+                      <div className="row text-center">
+                        <h3>Please login to continue</h3>
+                        <h5></h5>
+                      </div>
+                      <div className="row mb-5">
+                        <div className="col text-center">
+                          <h1 className="mt-2 logoFont">Curator</h1>
+                          <img
+                            src={Logo}
+                            alt="Nft"
+                            className="w-50 mt-4 rotateImage"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
